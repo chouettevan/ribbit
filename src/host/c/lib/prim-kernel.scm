752987d771635c6f9cc7c80d885763b5307f80e1
Carlos-Ariel Vanegas <vanegascarlos@proton.me>
2026-02-03 14:44:53 -0500

    remove debug prints

--- src/host/c/lib/prim-kernel.scm
+++ src/host/c/lib/prim-kernel.scm
@@ -20,6 +20,35 @@
 (define-primitive (inb port)
   "{
   PRIM1();
-  push2(TAG_NUM(inb((unsigned short)UNTAG(x))),PAIR_TAG); 
+  push2(TAG_NUM((int)inb((unsigned short)UNTAG(x))),PAIR_TAG); 
   break;
   }")
+
+(define-primitive (lor a b)
+  "{
+  PRIM2();
+  push2(TAG_NUM(UNTAG(x) | UNTAG(y)),PAIR_TAG); 
+  break;
+  }")
+
+(define-primitive (land a b)
+  "{
+  PRIM2();
+  push2(TAG_NUM(UNTAG(x) & UNTAG(y)),PAIR_TAG); 
+  break;
+  }")
+
+(define-primitive (lxor a b)
+  "{
+  PRIM2();
+  push2(TAG_NUM(UNTAG(x) ^ UNTAG(y)),PAIR_TAG); 
+  break;
+  }")
+
+(define-primitive (lshift a b)
+  "{
+  PRIM2();
+  push2(TAG_NUM(UNTAG(x) >> UNTAG(y)),PAIR_TAG); 
+  break;
+  }")
+
